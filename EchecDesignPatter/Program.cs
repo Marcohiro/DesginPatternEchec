1ff89f82eab0dcd40638f0c95768adbf8904f959
Marcohiro <petit.marcalain@yahoo.fr>
2020-03-24 12:36:39 +0100

    initial commit

---
+++ EchecDesignPatter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EchecDesignPatter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
